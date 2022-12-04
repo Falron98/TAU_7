fb0a0599ab370af89eadb227bb4fc60e9253fe8f
Falron98 <bartekkrystowski@wp.pl>
2022-12-04 03:23:12 +0100

    Init

--- Tests.py
+++ Tests.py
@@ -1,53 +1,38 @@
-from unittest import TestCase
-from hamcrest import assert_that, equal_to
+import unittest
 
-import hamcrest
+from assertpy import assert_that
 
-from hamcrest import *
-
-from main import Employee, ProductionWorker
+from main import Car
 
 
-class TestProductionWorker(TestCase):
+class CarTest(unittest.TestCase):
 
     def setUp(self):
-        self.temp = ProductionWorker("Kowalski", "Jan", 1, 12)
+        self.temp = Car(2010, "VW")
 
-    def test_True_get_lastname(self):
-        assert_that(self.temp.get_lastname(), not_none())
+    def test_Car_IsNotEmpty(self):
+        assert_that(self.temp).is_not_none()
 
-    def test_True_get_firstname(self):
-        assert_that(self.temp.get_firstname(), not_none())
+    def test_Car_BrakeShouldReturn0(self):
+        self.temp.brake()
+        assert_that(self.temp.get_speed()).is_zero()
 
-    def test_True_get_change_number(self):
-        assert_that(self.temp.get_change_number(), is_(1))
+    def test_Car_AccelerateShouldReturn5(self):
+        self.temp.accelerate()
+        assert_that(self.temp.get_speed()).is_equal_to(5)
 
-    def test_True_get_pay_hour(self):
-        assert_that(self.temp.get_pay_hour(), is_(12))
+    def test_Car_AttributesNotNone(self):
+        assert_that(self.temp).is_not_none()
 
-    def test_False_get_pay_hour(self):
-        assert_that(self.temp.get_pay_hour(), is_not(11))
+    def test_Car_2WrongAttributes(self):
+        self.assertRaises(Exception, Car("23432", 123))
+        assert_that(Car("23432", 123)).raises(Exception)
 
-    def test_True_set_lastname(self):
-        self.temp.set_lastname("Nowak")
-        assert_that(self.temp.get_lastname(), is_("Nowak"))
+    def test_Car_getSpeedGreaterThanMinus(self):
+        assert_that(self.temp.get_speed()).is_greater_than(-1)
 
-    def test_Fail_set_lastname_NotInt(self):
-        assert_that(self.temp.set_lastname(123), raises(Exception))
-
-    def test_True_set_change_number(self):
-        self.temp.set_change_number(2)
-        assert_that(self.temp.get_change_number(), less_than(3) and greater_than(0))
-
-    def test_Fail_set_change_number_MoreThan2(self):
-        assert_that(self.temp.set_change_number(5), raises(Exception))
-
-    def test_True_equal_object(self):
-        assert_that(self.temp, equal_to(ProductionWorker("Kowalski", "Jan", 1, 12)))
-
-    def test_Fail_set_pay_hour(self):
-        self.temp.set_pay_hour(2.22)
-        assert_that(self.temp.get_pay_hour(), is_not(int))
+    def test_Car_getSpeedisNotLessThan0(self):
+        assert_that(self.temp.get_speed()).is_not_in(-5,-4,-3,-2,-1)
 
     def tearDown(self):
         self.temp = None
--- main.py
+++ main.py
@@ -1,62 +1,22 @@
+import unittest
 
 
-class Employee:
-    def __init__(self, __lastname, __firstname):
-        if isinstance(__firstname, str) != True or isinstance(__lastname, str) != True:
-            raise Exception("Wrong input")
-        self.__lastname = __lastname
-        self.__firstname = __firstname
+class Car:
+    def __init__(self, year, model):
+        self.__year = year
+        self.__model = model
+        self.__speed = 0
 
-    def get_lastname(self):
-        return self.__lastname
+    def accelerate(self):
+        self.__speed += 5
 
-    def get_firstname(self):
-        return self.__firstname
+    def brake(self):
+        if 0 < self.__speed <= 5:
+            self.__speed = 0
+        elif self.__speed > 5:
+            self.__speed -= 5
 
-    def set_lastname(self, __lastname):
-        if isinstance(__lastname, str):
-            self.__lastname = __lastname
-        else:
-            raise Exception("Wrong input")
-
-    def set_firstname(self, __firstname):
-        if isinstance(__firstname, str):
-            self.__firstname = __firstname
-        else:
-            raise Exception("Wrong input")
+    def get_speed(self):
+        return self.__speed
 
 
-class ProductionWorker(Employee):
-
-    def __init__(self, __lastname, __firstname, __change_number, __pay_hour):
-        if isinstance(__change_number, int) is not True or \
-                isinstance(__pay_hour, int) is not True:
-            raise Exception("Wrong input")
-        elif __change_number < 0 or __change_number > 2:
-            raise Exception("Change number can be only 1 or 2")
-        elif __pay_hour <= 0:
-            raise Exception("Pay hour must be greater than 0")
-        try:
-            super().__init__(__lastname, __firstname)
-            self.__change_number = __change_number
-            self.__pay_hour = __pay_hour
-        except:
-            raise Exception
-
-    def get_change_number(self):
-        return self.__change_number
-
-    def get_pay_hour(self):
-        return self.__pay_hour
-
-    def set_change_number(self, __change_number):
-        if isinstance(__change_number, int) and 0 < __change_number <= 2:
-            self.__change_number = __change_number
-        else:
-            raise Exception("Wrong input")
-
-    def set_pay_hour(self, __pay_hour):
-        if isinstance(__pay_hour, int) and __pay_hour > 0:
-            self.__pay_hour = __pay_hour
-        else:
-            raise Exception("Wrong input")
